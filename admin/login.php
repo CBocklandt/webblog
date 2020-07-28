74b5e001281b26c65d433852b52f8bb9d66072aa
CBocklandt <58941284+CBocklandt@users.noreply.github.com>
2020-07-28 10:25:34 +0200

    login

--- admin/login.php
+++ admin/login.php
@@ -1,24 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require_once ("includes/header.php");?>
+<?php
+if ($session->is_signed_in()){
+    redirect("index.php");
+}
+if (isset($_POST['submit'])){
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
 
-<head>
+    $user_found = User::verify_user($username, $password);
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>SB Admin 2 - Login</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
+    if ($user_found){
+        $session->login($user_found);
+        redirect("index.php");
+    }else{
+        $the_message = "Your password or username FAILED";
+    }
+}else{
+    $username = "";
+    $password = "";
+}
+?>
 
 <body class="bg-gradient-primary">
 
@@ -39,12 +40,14 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user">
+                  <form class="user" method="post">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                        <label for="username">Username:</label>
+                      <input type="text" name="username" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                        <label for="password">Password:</label>
+                      <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
                     </div>
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
@@ -52,16 +55,9 @@
                         <label class="custom-control-label" for="customCheck">Remember Me</label>
                       </div>
                     </div>
-                    <a href="index.php" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
-                    <hr>
-                    <a href="index.php" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.php" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a>
+                    <div class="form-group">
+                        <input type="submit" name="submit" value="Aanmelden" class="btn btn-primary">
+                    </div>
                   </form>
                   <hr>
                   <div class="text-center">
